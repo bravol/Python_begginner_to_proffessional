0a8d506fcd558358974548277d4ce043751699ef
Brian Bravol <lumalabravo@gmail.com>
2024-09-25 14:53:13 +0300

    lesson 6 for loop finished

---
+++ lesson6(functions, code blocks, while loops)/task.py
@@ -0,0 +1,13 @@
+# print("hello")
+# num_char=len("hello")
+# print(num_char)
+
+# forming our won functions
+def my_function():
+    print("Hello world!")
+    print("Bye bye!")
+my_function()
+
+# PRACTISE WITH THESE GAMES
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
