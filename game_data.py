7a0a8a4136838c4258f3b3c37006300329e3dda0
Brian Bravol <lumalabravo@gmail.com>
2024-10-01 10:41:40 +0300

    divide the project into smaller chunks for easing the process

---
+++ game_data.py
@@ -0,0 +1,44 @@
+data = [
+    {
+        "name": "Instagram",
+        "follower_count": 346,
+        "description": "Social media platform",
+        "country": "United States"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 215,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 183,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 181,
+        "description": "Actor and Professional wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 174,
+        "description": "Musician and Actress  wr",
+        "country": "United States"
+    },
+    {
+        "name": "Kylie Jenner Johnson",
+        "follower_count": 172,
+        "description": "Reality TV personality and businesswoman and self-made billionaire",
+        "country": "United States"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 167,
+        "description": "Reality TV personality and businesswoman",
+        "country": "United States"
+    },
+]
---
+++ lesson14(Higher or lower game)/task.py
@@ -0,0 +1,19 @@
+# break down a bigger problem into small pieces
+# 1 Display art
+
+# Generate a random account from the game data
+
+# format the account data into printable format
+
+# Ask user for a guess.
+
+# check if the user is correct.
+
+## Get a follower account of each account
+## Use if statement to check if user is correct
+
+# give a user feedback on their guess
+
+# Make the game repeatable.
+
+#  Making accounts for position B become the next account at position A
