1a1b62526a77b024ec98a50c0695ff8af2fc31e4
Brian Bravol <lumalabravo@gmail.com>
2024-10-07 11:07:10 +0300

    controlling the movement of the snake

--- Intermediate_level/lesson20(Snake game)/main.py
+++ Intermediate_level/lesson20(Snake game)/main.py
@@ -1,22 +1,21 @@
-from turtle import Turtle, Screen
+from turtle import  Screen
 import time
+from snake import Snake
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width = 600, height = 600)
 screen.bgcolor('black')
 screen.title('My Snake Game')
 screen.tracer(0) #turn off the animation
 
-segments = []
+snake = Snake()
 
-#1. creating snake body
-starting_position = [(0,0),(-20,0),(-40,0)]
-for position in starting_position:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+# listeners
+screen.listen()
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.left,'Left')
+screen.onkey(snake.right,'Right')
 
 
 #2. move the snake
@@ -24,10 +23,6 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for segment_number in range(len(segments)-1, 0, -1):
-        new_x = segments[segment_number-1].xcor()
-        new_y = segments[segment_number-1].ycor()
-        segments[segment_number].goto(new_x,new_y)
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
