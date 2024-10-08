8720e029dcbcbaf14bf2ac8fc49132d0f73c57a1
Brian Bravol <lumalabravo@gmail.com>
2024-10-08 12:11:18 +0300

    random cars created

--- Intermediate_level/lesson23(Capstone Project - Turtle Crossing game)/main.py
+++ Intermediate_level/lesson23(Capstone Project - Turtle Crossing game)/main.py
@@ -9,12 +9,15 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up,'Up')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.1) # refresh the screen every 0.1 seconds
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move_cars()
